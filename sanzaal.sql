c623c5e96b9cf144089503d8a717e96b5676b387
unknown <ragaslagnad28@gmail.com>
2020-01-02 21:18:42 +0545

    oracle 11g

--- sanzaal.sql
+++ sanzaal.sql
@@ -3,34 +3,37 @@ SET SERVEROUTPUT ON;
 
 
 DECLARE
-    TYPE num_table IS TABLE OF VARCHAR2(20) INDEX BY VARCHAR2(20); -----index is indivisual number like index are 1,2,3,4 of 11234 contais ta count of number like  t(1) = 2
-    TYPE labell IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20); ------labell similar like num_table, index are same and contain "multiple" if count is <=2  else "single"
-    TYPE exp IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20);---------similar like labell, index are same as above, contains x,y,z respectively if "multiple"
-    x NUMBER ;                                                  ------- x is number for input
-    type namesarray IS VARRAY(5) OF VARCHAR2(10);           -----name contais x,y,z,w as array
-    name namesarray;                                ---declaring name as namearray
-    type numarray IS TABLE OF VARCHAR2(20);       --numarray contain indivisual digit of x like('1','2','3','4','5') of 12345 
-    char VARCHAR2(20);                    ----- declaring a character (it is latter used to kept char of x
-    numss numarray := numarray();       --- declaring numarray
-    n integer;         
+    TYPE num_table IS TABLE OF VARCHAR2(20) INDEX BY VARCHAR2(20);
+    TYPE labell IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20);
+    TYPE exp IS TABLE Of VARCHAR2(20) INDEX BY VARCHAR2(20);
+    x NUMBER ;
+    type namesarray IS VARRAY(5) OF VARCHAR2(10);
+    name namesarray;
+    type numarray IS TABLE OF VARCHAR2(20); 
+    char VARCHAR2(20);
+    numss numarray ;
+    n integer;
     countt integer;
-    tables num_table := num_table();
-    nums2 numarray := numarray();
-    nums3 numarray := numarray();
-    labels labell := labell();
-    expr exp := exp();
+    tables num_table;
+    nums2 numarray ;
+    nums3 numarray;
+    labels labell ;
+    expr exp ;
     patt VARCHAR2(20);
     
     
-FUNCTION getPattern(X IN NUMBER)RETURN VARCHAR2 IS patt VARCHAR2(20);   ------ function to return a pattern
+FUNCTION getPattern(X IN NUMBER)RETURN VARCHAR2 IS patt VARCHAR2(20);
     
-  BEGIN
-    name := namesarray('x','y','z','w');                              ---adding value to name array
-    char := to_char(X);                                            --------asigning a char of X to char
-    n := LENGTH(char);                                           ------lenght of char
+    BEGIN
+    
+   
+     name := namesarray('x','y','z','w');  
+   char := to_char(X);  
+   n := LENGTH(char);
     --DBMS_OUTPUT.PUT_LINE(LPAD(char,10));
+    numss := numarray();
     
-    for i in 1 .. n LOOP              ---------------------adding indivisual char of char to numss array
+    for i in 1 .. n LOOP 
         IF  not numss.exists(i)
         THEN
         numss.extend;
@@ -38,22 +41,13 @@ FUNCTION getPattern(X IN NUMBER)RETURN VARCHAR2 IS patt VARCHAR2(20);   ------ f
 
         --DBMS_OUTPUT.PUT_LINE(i);
         
-        END IF;       
+        END IF;
+        
     END LOOP;
-    
-    
-    
-    
-    
     --DBMS_OUTPUT.PUT_LINE(numss(2)); 
-    nums3 := numss;                 ---copying numss to nums3 for future used
+    nums3 := numss;
     nums2 := numss;
-    numss := numss MULTISET UNION DISTINCT nums2;   ----getting unique value of numss and assigning to numms again
-    
-    
-    
-    
-    
+    numss := numss MULTISET UNION DISTINCT nums2;
     for i in numss.FIRST .. numss.LAST LOOP
         --DBMS_OUTPUT.PUT_LINE(i);
         n := REGEXP_COUNT(char,numss(i));
@@ -100,7 +94,7 @@ FUNCTION getPattern(X IN NUMBER)RETURN VARCHAR2 IS patt VARCHAR2(20);   ------ f
     
     
 BEGIN 
-    x := 11223344;
+    x := 44556611;
    patt := getPattern(x); 
    DBMS_OUTPUT.PUT_LINE(x||'----------------------'||patt);
 END; 
